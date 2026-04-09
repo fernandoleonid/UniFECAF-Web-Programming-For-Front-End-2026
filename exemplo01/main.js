1e8244d2d158067ae521c3ac69dbea20573fe70f
Fernando Leonid <fernandoleonid@gmail.com>
2026-04-09 20:45:42 -0300

    exemplo01

---
+++ exemplo01/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// 1 - const    - imutável  /  escopo bloco 
+// 2 - let      - mutável   /  escopo bloco
+// 3 - var      - mutável   /  escopo globo
+{
+    var nome = 'Fernando Leonid'
+}
+nome = 'maria'
+console.log (nome)
